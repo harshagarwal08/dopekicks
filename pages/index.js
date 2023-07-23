6c8065d1797c82e3cabdf6e6606c97eb072c44f4
Harsh <harsh821agarwal@gmail.com>
2023-07-23 17:35:17 +0530

    feat: creating ProductCard component and category page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import HeroBanner from "@/components/HeroBanner";
 import Wrapper from "@/components/Wrapper";
-
+import ProductCard from "@/components/ProductCard";
 export default function Home() {
   return (
     <main>
@@ -13,6 +13,17 @@ export default function Home() {
           <div className="text-md md:text-xl">
           From Classic to Cutting-Edge: Discover the Hottest Sneaker Trends at Dopekicks and elevate your sneaker game!
           </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
         </div>
       </Wrapper>
     </main>
