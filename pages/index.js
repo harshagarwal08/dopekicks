dcd77d02c9065a86331ddcec43e769f769560ffc
Harsh <harsh821agarwal@gmail.com>
2023-07-23 17:07:04 +0530

    feat: create HeroBanner component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import HeroBanner from "@/components/HeroBanner";
+import Wrapper from "@/components/Wrapper";
+
 export default function Home() {
   return (
-    <main className="h-[100vh]">Home Page</main>
+    <main>
+      <HeroBanner />
+      <Wrapper>
+        <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+          Your Ultimate Sneaker Destination
+          </div>
+          <div className="text-md md:text-xl">
+          From Classic to Cutting-Edge: Discover the Hottest Sneaker Trends at Dopekicks and elevate your sneaker game!
+          </div>
+        </div>
+      </Wrapper>
+    </main>
   )
 }
